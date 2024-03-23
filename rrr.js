cc674449c515328b4935aa3c7a88c190f932ef9b
Prasad-1999 <92925789+Prasad-1999@users.noreply.github.com>
2024-03-23 11:59:16 +0530

    ram

---
+++ rrr.js
@@ -0,0 +1,58 @@
+// var pin=1234;
+// var pins=1234;
+// var pass=555;
+// var passwd=666;
+// if(pin==pins)  {
+//     if(pass==passwd){
+//     console.log("Enter pin is correct")
+//     console.log("Welcome to SBI ATM")
+//     }else{
+//     console.log("Enter pin is in correct")
+//     }
+// }else{
+//     console.log("Invalid data ")
+// }
+
+// var marks = 80;
+// var grade ;
+// switch(true){
+//     case (marks <= 100 && marks >= 90):
+//         grade = "A";
+//         console.log("The marks is:"+marks+ "and the grade is"+grade)
+//         break;
+//     case (marks <= 89 && marks >= 80):
+//         grade = "B";
+//         console.log("The  marks is:"+marks+ "and the grade is"+grade)
+//         break;
+//     case (marks <= 79 && marks >= 70):
+//         grade = "C";
+//         console.log("The  marks is:"+marks+"and the grade is"+grade)
+//         break;
+//     case (marks <= 69 && marks >= 60):
+//         grade = "D";
+//         console.log("The  marks is:"+marks+"and the grade is"+grade)
+//         break;
+//     case (marks <= 59 && marks >= 0):
+//         grade = "F";
+//         console.log("The  marks is:"+marks+"and the grade is"+grade)
+//         break;
+//     default:
+//         console.log("Enter valid marks")
+// }        
+
+// anynomous function
+// let x= function(n1,n2){
+//     return n1+n2
+// }
+// console.log(x(20,20));
+
+// var a=[0,1,2,3]
+// a.push()
+// console.log(a)
+
+var i=1
+while(i<10){
+    if (i%2==1)
+    console.log(i);
+    i++;
+}
---
+++ rrr3.js
@@ -0,0 +1,28 @@
+// It gives index value of element as output
+// var fruits=['mango','bananas','apple','kiwi']
+// for (value in fruits){
+//     console.log(value)
+// }
+
+// It gives elements as output
+// var fruits=['mango','bananas','apple','kiwi']
+// for (value of fruits){
+//     console.log(value)
+// }
+
+// It gives the both index numbers and values
+// var fruits=['mango','bananas','apple','kiwi']
+// for (value in fruits){
+//     console.log(value + ":"+ fruits[value])
+// }
+
+// var movies=['rrr','bahubali','kgf']
+//     // console.log(movies)
+//     document.write(movies)
+
+// var movies=['rrr','bahubali','kgf','maharshi']
+// movies.pop()
+// console.log(movies)
+
+var movies=['rrr','Bahubali','kgf','maharshi']
+console.log(movies.slice(1,3))
